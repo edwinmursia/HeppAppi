8ea049e4ccd91fbfebe2bbe46f49e23d9eba9432
Akseli Rautionmaa <akseli.rautionmaa@gmail.com>
2021-11-26 15:42:46 +0200

    3 new screens and a new picture to go with it

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import StartingScreen from './screens/StartingScreen';
 import LogInScreen from './screens/LogInScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import AddHorseScreen from './screens/AddHorseScreen';
-
+import EditHorseScreen from './screens/EditHorseScreen';
+import UserSettingsScreen from './screens/UserSettingsScreen';
+import SettingsScreen from './screens/SettingsScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="StartingScreen" >
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="SetttingsScreen" >
         <Stack.Screen name="StartingScreen" component={StartingScreen} />
         <Stack.Screen name="LogInScreen" component={LogInScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen name="AddHorseScreen" component={AddHorseScreen} />
+        <Stack.Screen name="EditHorseScreen" component={EditHorseScreen} />
+        <Stack.Screen name="UserSettingsScreen" component={UserSettingsScreen} />
+        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/UserSettingsScreen.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { SafeAreaView, StyleSheet, Image, TextInput, Pressable, Text, TouchableOpacity, View } from 'react-native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const UserSettingsScreen = () => {
+
+    //console.log(RadioButton);
+    //const [val, setVal] = useState("-");
+
+    function pressCircle(i) {
+        setVal(i);
+    }
+
+    const navigation = useNavigation()
+    const goBack = () => {
+        navigation.goBack()
+    }
+
+    return (
+        <SafeAreaView style={styles.container} >
+            <TouchableOpacity style={styles.IconWrapper} onPress={goBack}>
+                <Icon name='chevron-back' size={30} color="#000" style={{ marginRight: -22 }} />
+                <Icon name='chevron-back' size={30} color="#000" />
+                <Text style={{ fontSize: 15, fontWeight: 'bold' }} >Takaisin</Text>
+            </TouchableOpacity>
+            <Image source={require('../images/wawingguy.jpg')} style={{position: 'absolute', height: 100, width: 100, top:98, left:140, right:100, bottom:0}} />
+            <Text>Placeholder</Text>
+            <TextInput style={styles.input} placeholder="Etunimi" />
+            <TextInput style={styles.input} placeholder="Sukunimi" />
+            <TextInput style={styles.input} placeholder="Vanha salasana?" />
+            <TextInput style={styles.input} placeholder="Uusi salasana" secureTextEntry={true}/>
+            <TextInput style={styles.input} placeholder="Vahvista uusi salasana" secureTextEntry={true}/>
+            <Pressable style={styles.buttonLogIn} >
+                <Text style={styles.text} >Päivitä tiedot</Text>
+                </Pressable>
+        </SafeAreaView>
+    )
+}
+
+export default UserSettingsScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    IconWrapper: {
+        flexDirection: 'row',
+        height: 40,
+        bottom: '25%',
+        right: 90,
+        alignItems: 'center'
+    },
+    buttonLogIn: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#0b6122',
+        height: 45,
+        width: 240,
+        marginTop: 5,
+        borderRadius: 20,
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    input: {
+        height: 50,
+        margin: 6,
+        borderWidth: 2,
+        borderRadius: 10,
+        width: '70%',
+        paddingLeft: 10
+    },
+    input1: {
+        height: 250,
+        margin: 6,
+        borderWidth: 2,
+        borderRadius: 10,
+        width: '70%',
+        paddingLeft: 10
+    },
+    buttonRegister: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#66CE26',
+        height: 35,
+        width: 220,
+        marginTop: 30,
+        borderRadius: 20,
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    text: {
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    passwordText: {
+        fontWeight: 'bold',
+        margin: 10
+    },
+});

31da470d4b88e80431228b9c34cda2ba581256da
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-12-12 22:03:36 +0200

    Added the name to the MainScreen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -22,23 +22,33 @@ const MainScreen = () => {
 
     const isFocused = useIsFocused();
     const [time, setTime] = useState('');
-    const [date, setDate] = useState('')
+    const [date, setDate] = useState('');
+    const [name, setName] = useState('');
 
-    const getTime = () => {
-        AsyncStorage.getItem('time')
-            .then((time) => {
-                setTime(time);
-            }, [isFocused])
-    }
-    const getDate = () => {
-        AsyncStorage.getItem('dayOne')
-            .then((date) => {
-                setDate(date);
-            }, [isFocused])
-    }
+    const getAllValues = () => {
+        const getTime = () => {
+            AsyncStorage.getItem('time')
+                .then((time) => {
+                    setTime(time);
+                }, [isFocused])
+        }
+        const getDate = () => {
+            AsyncStorage.getItem('dayOne')
+                .then((date) => {
+                    setDate(date);
+                }, [isFocused])
+        }
+        const getName = () => {
+            AsyncStorage.getItem('nameInput')
+                .then((name) => {
+                    setName(name);
+                }, [isFocused])
+        }
 
-    getTime.apply(time)
-    getDate.apply(date)
+        getTime.apply(time)
+        getDate.apply(date)
+        getName.apply(name)
+    }
 
     const navFunction = () => {
         navigation.navigate('ReservationScreen')
@@ -66,9 +76,9 @@ const MainScreen = () => {
     }
 
     return (
-        <SafeAreaView style={styles.container} >
+        <SafeAreaView style={styles.container} onLayout={getAllValues} >
             <View style={styles.upperContainer}>
-                <Text style={styles.welcomeText} >Tervetuloa Etunimi!</Text>
+                <Text style={styles.welcomeText} >Tervetuloa {name.replace(/['"]+/g, '')}!</Text>
                 <Icon name='ios-settings-outline' size={20} style={{ paddingRight: 25 }} color='#fff' onPress={navSettings} />
             </View>
             <Text style={{ fontSize: 11, width: '90%', textAlign: 'center', textTransform: 'none', color: 'grey', paddingTop: 20, paddingBottom: 10 }} >
